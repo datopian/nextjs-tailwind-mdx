52e69474588ded437223a21f9eb8f5a81faf661d
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-23 18:53:47 +0200

    [config,theme,#1][m]: footer uses siteConfig values to generate "Created by ..." and also use siteConfig (e.g. title, tagline) on front page.
    * Also tweak favicon to be an emoji

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,14 @@ import Link from 'next/link'
 import Head from 'next/head'
 
 import Nav from './Nav'
+import siteConfig from '../config/siteConfig'
 
 export default function Layout({ children, title='' }) {
   return (
     <>
       <Head>
-        <title>{title}{title && '- '}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title}{title && '- '}{siteConfig.title}</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
@@ -16,15 +17,17 @@ export default function Layout({ children, title='' }) {
       <main>
         {children}
       </main>
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://datopian.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="https://playbook.datopian.com/img/datopian-dark-logotype.svg" alt="Datopian Logo" className="h-12 block" />
-        </a>
+      <footer className="flex items-center justify-center w-full h-24 border-t mt-16">
+        <p className="flex items-center justify-center">
+          Created by
+          <a
+            href={siteConfig.authorUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={siteConfig.authorLogo} alt="{siteConfig.author} Logo" className="ml-2 h-6 block" />
+          </a>
+        </p>
       </footer>
     </>
   )
--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,7 +1,12 @@
 const siteConfig = {
-  title: 'Next.js Starter Template',
-  author: 'Rufus Pollock',
-  ownerLogo: '',
+  title: 'Datopian Next.js Template',
+  tagline: 'A starter template for Next.JS with Tailwind and MDX',
+  description: '',
+  author: 'Datopian',
+  // logo image
+  authorLogo: 'https://playbook.datopian.com/img/datopian-dark-logotype.svg',
+  // url to author
+  authorUrl: 'https://datopian.com/',
   // Google analytics key e.g. G-XXXX
   analytics: '',
 }
