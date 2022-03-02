db00a8cf0b6f233b1b27191e4bdcf32eab4788a8
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-02 13:40:23 +0100

    [pages/_app][xs]: remove debug info.

--- pages/_app.js
+++ pages/_app.js
@@ -25,10 +25,6 @@ function MyApp({ Component, pageProps }) {
   }
   // end Google Analytics
   
-  const router = useRouter()
-  console.log(pageProps)
-  console.log(router.pathname)
-
   return (
     <>
       <DefaultSeo
